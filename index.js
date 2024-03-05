0c2436f8c11f17301ff50bf5e833f9f9e688e1ef
JimTK16 <longduy88@gmail.com>
2024-03-05 22:05:28 +1100

    first commit

---
+++ index.js
@@ -0,0 +1,79 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+let notes = [
+  {
+    id: 1,
+    content: 'HTML is easy',
+    important: true
+  },
+  {
+    id: 2,
+    content: 'Browser can execute only Javascript',
+    important: false
+  },
+  {
+    id: 3,
+    content: 'GET and POST are the most important methods of HTTP protocol',
+    important: true
+  }
+]
+
+//allow requests from all origin
+app.use(cors())
+
+//activate the json-parser of express
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1><p>Testing4</p>')
+})
+
+app.get('/api/notes', (req, res) => {
+  res.json(notes)
+})
+
+app.get('/api/notes/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const note = notes.find((n) => n.id === id)
+  if (note) {
+    res.json(note)
+  } else {
+    // res.statusMessage = 'Item not found'
+    res.status(204).end()
+  }
+})
+
+app.delete('/api/notes/:id', (req, res) => {
+  const id = Number(req.params.id)
+  notes = notes.filter((n) => n.id !== id)
+
+  res.status(204).end()
+})
+
+const generateId = () => {
+  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0
+  return maxId + 1
+}
+
+app.post('/api/notes', (req, res) => {
+  const body = req.body
+  if (!body.content) {
+    return res.status(400).json({ error: 'content missing' })
+  }
+
+  const note = {
+    content: body.content,
+    important: Boolean(body.important) || false,
+    id: generateId()
+  }
+
+  notes = notes.concat(note)
+  res.json(note)
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
