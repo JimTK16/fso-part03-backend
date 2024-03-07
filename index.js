9101bf55615eac9c542758e4a66be7311f6379c6
JimTK16 <longduy88@gmail.com>
2024-03-07 11:28:41 +1100

    uibuild

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require('dotenv').config() // make variables in .env file available globally
 const express = require('express')
 const cors = require('cors')
 const app = express()
+const Note = require('./models/note')
 
 let notes = [
   {
@@ -34,18 +36,11 @@ app.get('/', (req, res) => {
 })
 
 app.get('/api/notes', (req, res) => {
-  res.json(notes)
+  Note.find({}).then((result) => res.json(result))
 })
 
 app.get('/api/notes/:id', (req, res) => {
-  const id = Number(req.params.id)
-  const note = notes.find((n) => n.id === id)
-  if (note) {
-    res.json(note)
-  } else {
-    // res.statusMessage = 'Item not found'
-    res.status(204).end()
-  }
+  Note.findById(req.params.id).then((note) => res.json(note))
 })
 
 app.delete('/api/notes/:id', (req, res) => {
@@ -66,17 +61,17 @@ app.post('/api/notes', (req, res) => {
     return res.status(400).json({ error: 'content missing' })
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
-    important: Boolean(body.important) || false,
-    id: generateId()
-  }
+    important: body.important || false
+  })
 
-  notes = notes.concat(note)
-  res.json(note)
+  note.save().then((savedNote) => {
+    res.json(savedNote)
+  })
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
